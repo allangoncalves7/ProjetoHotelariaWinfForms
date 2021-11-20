4c891ff3d98a5011e41b0e1164416ab53d475056
vitorlSilva <vitor.lourenco@opencircle.com.br>
2021-11-20 02:08:03 -0300

    test

--- ProjetoPim/models/Hospede.cs
+++ ProjetoPim/models/Hospede.cs
@@ -8,6 +8,7 @@ namespace ProjetoPim.models
 {
     public class Hospede
     {
+        public Hospede() { }
         public int IdHospede { get; set; }
         public string Nome { get; set; }
         public string Cpf { get; set; }
